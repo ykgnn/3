21690318d244d66e2073f22054da4b3996570956
EC2 Default User <ec2-user@ip-172-31-21-146.ec2.internal>
2023-01-03 08:56:14 +0000

    [finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,6 +30,12 @@ redirect_to list_path(list.id)
     redirect_to list_path(list.id)  
   end
   
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
+  
   private
   # ストロングパラメータ
   def list_params
