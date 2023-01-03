900d4904dd67aeef16487dea32b51b54804ba404
EC2 Default User <ec2-user@ip-172-31-21-146.ec2.internal>
2023-01-03 08:43:22 +0000

    [finish]3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -33,6 +33,6 @@ redirect_to list_path(list.id)
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)  
   end
 end
