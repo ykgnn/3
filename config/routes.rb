e6e262755869bd7b6581a6ab049cfb2676b27877
EC2 Default User <ec2-user@ip-172-31-21-146.ec2.internal>
2023-01-03 08:03:42 +0000

    [finish]3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
+  get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
+  get 'lists' => 'lists#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
